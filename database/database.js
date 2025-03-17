8303ea9b7bc1f3dc2817fbd64814efc28d898b6c
Fábio de Alencar Rocha <otymus27@gmail.com>
2025-03-17 15:31:33 -0300

    Alteração no arquivo ENV - adição de conexão remota

--- database/database.js
+++ database/database.js
@@ -1,21 +1,5 @@
 import mongoose, { connect } from "mongoose";
 
-// mongoose.connect('mongodb://admin:123@localhost:27017/db_consultorio?authSource=admin');
-
-// const db = mongoose.connection;
-
-// db.on('Erro: ', console.error.bind(console, 'Erro de conexão!!!'));
-
-// db.once(
-//      'open', function () {
-//           console.log('Conexão com banco de dados feita com sucesso!');
-//      }
-// )
-
-
-
-
-
 const db = () => {
 
      console.log("Esperando conectar ao banco de dados!");
